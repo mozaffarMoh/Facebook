c3a19954721142e2e6064dd49b7cdc835f8fd9f0
Mozaffar <FERAWWWESS@gmail.com>
2024-02-05 10:30:29 +0300

    final facebood loading images

--- src/components/Home/home-posts/home-posts.tsx
+++ src/components/Home/home-posts/home-posts.tsx
@@ -21,6 +21,7 @@ const HomePosts = ({ setShowBrightness }: any) => {
   const [userComment, setUserComment] = React.useState("");
   const [editCommentDialog, setEditCommentDialog] = React.useState(false);
   const [editCommentIndex, setEditCommentIndex] = React.useState(0);
+  const [loadedImagesArray, setLoadedImagesArray]: any = React.useState([]);
   const editCommentRef: any = React.useRef(null);
 
   const createPostNavigation = [
@@ -38,6 +39,16 @@ const HomePosts = ({ setShowBrightness }: any) => {
     },
   ];
   isScreen666 && createPostNavigation.pop();
+
+  /* wait to load profiles images */
+  const handleLoadedImage = (id: number) => {
+    if (loadedImagesArray && !loadedImagesArray?.includes(id)) {
+      setLoadedImagesArray((prevArray: any) => {
+        const newArray = [...prevArray, id];
+        return newArray;
+      });
+    }
+  };
 
   /* Show Create post and show brightness */
   const handleCreatePost = () => {
@@ -196,7 +207,39 @@ const HomePosts = ({ setShowBrightness }: any) => {
               {/* Header */}
               <div className="post-header justify-between items-start px-5">
                 <div className="flex">
-                  <img src={post.pageImg} alt="Image" />
+                  {!loadedImagesArray.includes(index) && (
+                    <div className="post-page-image-alt flexCenter">
+                      <svg
+                        className="text-gray-300 animate-spin"
+                        viewBox="0 0 64 64"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                      >
+                        <path
+                          d="M32 3C35.8083 3 39.5794 3.75011 43.0978 5.20749C46.6163 6.66488 49.8132 8.80101 52.5061 11.4939C55.199 14.1868 57.3351 17.3837 58.7925 20.9022C60.2499 24.4206 61 28.1917 61 32C61 35.8083 60.2499 39.5794 58.7925 43.0978C57.3351 46.6163 55.199 49.8132 52.5061 52.5061C49.8132 55.199 46.6163 57.3351 43.0978 58.7925C39.5794 60.2499 35.8083 61 32 61C28.1917 61 24.4206 60.2499 20.9022 58.7925C17.3837 57.3351 14.1868 55.199 11.4939 52.5061C8.801 49.8132 6.66487 46.6163 5.20749 43.0978C3.7501 39.5794 3 35.8083 3 32C3 28.1917 3.75011 24.4206 5.2075 20.9022C6.66489 17.3837 8.80101 14.1868 11.4939 11.4939C14.1868 8.80099 17.3838 6.66487 20.9022 5.20749C24.4206 3.7501 28.1917 3 32 3L32 3Z"
+                          stroke="currentColor"
+                          stroke-width="5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        ></path>
+                        <path
+                          d="M32 3C36.5778 3 41.0906 4.08374 45.1692 6.16256C49.2477 8.24138 52.7762 11.2562 55.466 14.9605C58.1558 18.6647 59.9304 22.9531 60.6448 27.4748C61.3591 31.9965 60.9928 36.6232 59.5759 40.9762"
+                          stroke="currentColor"
+                          stroke-width="5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          className="text-blue-500"
+                        ></path>
+                      </svg>
+                    </div>
+                  )}
+                  <img
+                    src={post.pageImg}
+                    alt="Image"
+                    onLoad={() => handleLoadedImage(index)}
+                  />
                   <div className="flex flex-col justify-center">
                     <h2>{post.title}</h2>
                     <p>{post.date}</p>
@@ -225,17 +268,57 @@ const HomePosts = ({ setShowBrightness }: any) => {
               {/* Media */}
               {(post.mediaImage || post.mediaImageURL || post.mediaVideo) && (
                 <div className="post-media w-100 flex ">
+                  {!loadedImagesArray.includes((index + 1) * 10) && (
+                    <div className="post-media-alt flexCenter">
+                      <svg
+                        className="text-gray-300 animate-spin"
+                        viewBox="0 0 64 64"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="70"
+                        height="70"
+                      >
+                        <path
+                          d="M32 3C35.8083 3 39.5794 3.75011 43.0978 5.20749C46.6163 6.66488 49.8132 8.80101 52.5061 11.4939C55.199 14.1868 57.3351 17.3837 58.7925 20.9022C60.2499 24.4206 61 28.1917 61 32C61 35.8083 60.2499 39.5794 58.7925 43.0978C57.3351 46.6163 55.199 49.8132 52.5061 52.5061C49.8132 55.199 46.6163 57.3351 43.0978 58.7925C39.5794 60.2499 35.8083 61 32 61C28.1917 61 24.4206 60.2499 20.9022 58.7925C17.3837 57.3351 14.1868 55.199 11.4939 52.5061C8.801 49.8132 6.66487 46.6163 5.20749 43.0978C3.7501 39.5794 3 35.8083 3 32C3 28.1917 3.75011 24.4206 5.2075 20.9022C6.66489 17.3837 8.80101 14.1868 11.4939 11.4939C14.1868 8.80099 17.3838 6.66487 20.9022 5.20749C24.4206 3.7501 28.1917 3 32 3L32 3Z"
+                          stroke="currentColor"
+                          stroke-width="5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        ></path>
+                        <path
+                          d="M32 3C36.5778 3 41.0906 4.08374 45.1692 6.16256C49.2477 8.24138 52.7762 11.2562 55.466 14.9605C58.1558 18.6647 59.9304 22.9531 60.6448 27.4748C61.3591 31.9965 60.9928 36.6232 59.5759 40.9762"
+                          stroke="currentColor"
+                          stroke-width="5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          className="text-blue-500"
+                        ></path>
+                      </svg>
+                    </div>
+                  )}
                   {post.mediaImage && (
-                    <img src={post.mediaImage} alt="" loading="lazy" />
+                    <img
+                      src={post.mediaImage}
+                      alt=""
+                      loading="lazy"
+                      onLoad={() => handleLoadedImage((index + 1) * 10)}
+                    />
                   )}
                   {post.mediaImageURL && (
                     <img
                       src={URL.createObjectURL(post.mediaImageURL)}
                       alt=""
                       loading="lazy"
+                      onLoad={() => handleLoadedImage((index + 1) * 10)}
                     />
                   )}
-                  {post.mediaVideo && <video src={post.mediaVideo} controls />}
+                  {post.mediaVideo && (
+                    <video
+                      src={post.mediaVideo}
+                      controls
+                      onLoad={() => handleLoadedImage((index + 1) * 10)}
+                    />
+                  )}
                 </div>
               )}
 
--- src/components/Home/home-stories/story-details/story-details.tsx
+++ src/components/Home/home-stories/story-details/story-details.tsx
@@ -34,7 +34,18 @@ const StoryDetails = () => {
   const [pauseStory, setPauseStory] = React.useState(false);
   const [resetStory, setResetStory] = React.useState(false);
   const [reactionState, setReactionState] = React.useState("");
+  const [loadedImagesArray, setLoadedImagesArray]: any = React.useState([]);
   const [loaded, setLoaded] = React.useState(false);
+
+  /* Wait To load Image */
+  const handleLoadedImage = (id: number) => {
+    if (loadedImagesArray && !loadedImagesArray?.includes(id)) {
+      setLoadedImagesArray((prevArray: any) => {
+        const newArray = [...prevArray, id];
+        return newArray;
+      });
+    }
+  };
 
   /* Handle Close */
   const handleClose = () => {
@@ -175,7 +186,15 @@ const StoryDetails = () => {
                     }`}
                   >
                     <div className="contacts-item-image">
-                      <img src={item.img} alt="" />
+                      {!loadedImagesArray.includes(item.id * 10) && (
+                        <div className="story-image-page-posters-alt"></div>
+                      )}
+
+                      <img
+                        src={item.img}
+                        alt=""
+                        onLoad={() => handleLoadedImage(item.id * 10)}
+                      />
                     </div>
                   </div>
                   <div className="poster-text flex flex-col px-3">
@@ -247,7 +266,16 @@ const StoryDetails = () => {
                     />
                     <div className="story-header">
                       <div className="header-content">
-                        <img src={story.img} alt="" />
+                        {!loadedImagesArray.includes(story.id) && (
+                          <div className="story-image-page-alt"></div>
+                        )}
+
+                        <img
+                          src={story.img}
+                          alt=""
+                          onLoad={() => handleLoadedImage(story.id)}
+                        />
+
                         <div>
                           <h1>
                             {!(story.title.length >= 8)
@@ -404,7 +432,7 @@ const StoryDetails = () => {
                     </div>
                   </div>
                   <div className="media">
-                    {!loaded && (
+                    {loaded && (
                       <div
                         className="on-load-image"
                         style={{
@@ -412,7 +440,7 @@ const StoryDetails = () => {
                         }}
                       >
                         {" "}
-                        {story.text && (
+                        {story.text ? (
                           <h1
                             style={{
                               color: "white",
@@ -421,6 +449,31 @@ const StoryDetails = () => {
                           >
                             {story.text}
                           </h1>
+                        ) : (
+                          <svg
+                            className="text-gray-300 animate-spin"
+                            viewBox="0 0 64 64"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="44"
+                            height="44"
+                          >
+                            <path
+                              d="M32 3C35.8083 3 39.5794 3.75011 43.0978 5.20749C46.6163 6.66488 49.8132 8.80101 52.5061 11.4939C55.199 14.1868 57.3351 17.3837 58.7925 20.9022C60.2499 24.4206 61 28.1917 61 32C61 35.8083 60.2499 39.5794 58.7925 43.0978C57.3351 46.6163 55.199 49.8132 52.5061 52.5061C49.8132 55.199 46.6163 57.3351 43.0978 58.7925C39.5794 60.2499 35.8083 61 32 61C28.1917 61 24.4206 60.2499 20.9022 58.7925C17.3837 57.3351 14.1868 55.199 11.4939 52.5061C8.801 49.8132 6.66487 46.6163 5.20749 43.0978C3.7501 39.5794 3 35.8083 3 32C3 28.1917 3.75011 24.4206 5.2075 20.9022C6.66489 17.3837 8.80101 14.1868 11.4939 11.4939C14.1868 8.80099 17.3838 6.66487 20.9022 5.20749C24.4206 3.7501 28.1917 3 32 3L32 3Z"
+                              stroke="currentColor"
+                              stroke-width="5"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            ></path>
+                            <path
+                              d="M32 3C36.5778 3 41.0906 4.08374 45.1692 6.16256C49.2477 8.24138 52.7762 11.2562 55.466 14.9605C58.1558 18.6647 59.9304 22.9531 60.6448 27.4748C61.3591 31.9965 60.9928 36.6232 59.5759 40.9762"
+                              stroke="currentColor"
+                              stroke-width="5"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              className="text-blue-500"
+                            ></path>
+                          </svg>
                         )}
                       </div>
                     )}
